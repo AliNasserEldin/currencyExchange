b16fc25134d44306f617ae0ac3920b9bca5094b2
alinasser <121856357+AliNasserEldin@users.noreply.github.com>
2023-08-28 10:47:05 +0300

    Update CachingConfig.java

--- src/main/java/com/gp/currencyexchange/config/CachingConfig.java
+++ src/main/java/com/gp/currencyexchange/config/CachingConfig.java
@@ -1,23 +1,20 @@
- package com.gp.currencyexchange.config;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
- import org.springframework.cache.CacheManager;
- import org.springframework.cache.annotation.EnableCaching;
- import org.springframework.cache.concurrent.ConcurrentMapCache;
- import org.springframework.cache.support.SimpleCacheManager;
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
-
- import java.util.Arrays;
-
- @Configuration
- @EnableCaching
- public class CachingConfig {
-
-     @Bean
-     public CacheManager cacheManager() {
-         SimpleCacheManager cacheManager = new SimpleCacheManager();
-         ConcurrentMapCache currenciesCache = new ConcurrentMapCache("CurrenciesExchange");
-         cacheManager.setCaches(Arrays.asList(currenciesCache));
-         return cacheManager;
-     }
- }
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
